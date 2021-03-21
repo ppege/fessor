93ac47f3e89718d87c46e9513e054065d59f3ea3
nangurepo <nanguthenangu@gmail.com>
2021-03-21 03:52:58 +0100

    deez

--- school/scanschedule.py
+++ school/scanschedule.py
@@ -1,11 +1,21 @@
 import schedule
 import time
-import lektiescanner
+from lektiescanner import lektiescan
 
-def job():
-    lektescanner.lektiescan()
+def scan():
+    beskrivelse, begivenhed, tidspunkt, files, fileNames, author = lektiescan()
+    f = open("scanResults.txt", "r")
+    previousResults = f.read()
+    f.close()
+    if str(begivenhed) == previousResults:
+    	return
+    else:
+    	f = open("scanResults.txt", "w")
+    	f.write(str(begivenhed))
+    	f.close
+    	
 
-schedule.every(20).minutes.do(job)
+schedule.every(20).minutes.do(scan)
 
 while True:
     schedule.run_pending()
