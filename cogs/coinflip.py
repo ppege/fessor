c27538c53d6c3006d1bd5ca8004f39391c3d2fc8
NanguRepo <nanguthenangu@gmail.com>
2021-05-23 04:10:41 +0200

    added exception handling to cog loader, added reaction buttons to coinflip

--- cogs/coinflip.py
+++ cogs/coinflip.py
@@ -6,14 +6,25 @@ class Coinflip(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=['cf', 'flip'])
     async def coinflip(self, ctx):
-        result = random.uniform(0, 1)
-        if result > 0.5:
-            output = "Heads!"
-        else:
-            output = "Tails!"
-        await ctx.send(embed=discord.Embed(title=output, description=f"Float: {str(result)}", color=0xFF0000))
+        def getResult():
+            result = random.uniform(0, 1)
+            if result > 0.5:
+                output = "Heads!"
+            else:
+                output = "Tails!"
+            return output, result
+        output, result = getResult()
+        message = await ctx.send(embed=discord.Embed(title=output, description=f"Float: {str(result)}", color=0xFF0000))
+        await message.add_reaction('🔄')
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == '🔄'
+
+        while(True):
+            reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+            output, result = getResult()
+            await message.edit(embed=discord.Embed(title=output, description=f"Float: {str(result)}", color=0xFF0000))
 
 def setup(bot):
     bot.add_cog(Coinflip(bot))
--- ctx.py
+++ ctx.py
@@ -82,32 +82,49 @@ async def on_command_error(ctx, error):
 @functions.utils.admin()
 @bot.command()
 async def cogs(ctx, action, cog):
-  if action == "load":
-    message = await ctx.send(embed=discord.Embed(title=f'Loading {cog}...', description=''))
-    bot.load_extension(f'cogs.{cog}')
-    await message.edit(embed=discord.Embed(title=f'{cog} loaded.', description='', color=0xFF0000))
-  elif action == "unload":
-    message = await ctx.send(embed=discord.Embed(title=f'Unloading {cog}...', description=''))
-    bot.unload_extension(f'cogs.{cog}')
-    await message.edit(embed=discord.Embed(title=f'{cog} unloaded.', description='', color=0xFF0000))
-  elif action == "reload":
-    if cog != "all":
-        message = await ctx.send(embed=discord.Embed(title=f'Reloading {cog}...', description=''))
-        bot.unload_extension(f'cogs.{cog}')
-        bot.load_extension(f'cogs.{cog}')
-        await message.edit(embed=discord.Embed(title=f'{cog} reloaded.', description='', color=0xFF0000))
-    else:
-        message = await ctx.send(embed=discord.Embed(title=f'Reloading all cogs...', description=''))
-        fileAmount = len(os.listdir('./cogs'))
-        i = 0
-        for filename in os.listdir('./cogs'):
-            i = i + 1
-            if filename.endswith('.py'):
-                bot.unload_extension(f"cogs.{filename[:-3]}")
-                bot.load_extension(f"cogs.{filename[:-3]}")
-            if i % 5 == 0:
-                await message.edit(embed=discord.Embed(title=f'{i} cogs reloaded.'))
-        await message.edit(embed=discord.Embed(title=f'All {i} cogs reloaded.', description='', color=0xFF0000))
+    try:
+        if action == "load":
+            message = await ctx.send(embed=discord.Embed(title=f'Loading {cog}...', description=''))
+            bot.load_extension(f'cogs.{cog}')
+            await message.edit(embed=discord.Embed(title=f'{cog} loaded.', description='', color=0xFF0000))
+        elif action == "unload":
+            message = await ctx.send(embed=discord.Embed(title=f'Unloading {cog}...', description=''))
+            bot.unload_extension(f'cogs.{cog}')
+            await message.edit(embed=discord.Embed(title=f'{cog} unloaded.', description='', color=0xFF0000))
+        elif action == "reload":
+            if cog != "all":
+                message = await ctx.send(embed=discord.Embed(title=f'Reloading {cog}...', description=''))
+                bot.reload_extension(f'cogs.{cog}')
+                await message.edit(embed=discord.Embed(title=f'{cog} reloaded.', description='', color=0xFF0000))
+            else:
+                message = await ctx.send(embed=discord.Embed(title=f'Reloading all cogs...', description=''))
+                fileAmount = len(os.listdir('./cogs'))
+                i = 0
+                for filename in os.listdir('./cogs'):
+                    i = i + 1
+                    if filename.endswith('.py'):
+                        bot.reload_extension(f"cogs.{filename[:-3]}")
+                    if i % 5 == 0:
+                        await message.edit(embed=discord.Embed(title=f'{i} cogs reloaded.'))
+                await message.edit(embed=discord.Embed(title=f'All {i} cogs reloaded.', description='', color=0xFF0000))
+    except commands.ExtensionAlreadyLoaded:
+        await ctx.send(embed=discord.Embed(title=f'{cog} is already loaded.'))
+    except commands.ExtensionNotLoaded:
+        await ctx.send(embed=discord.Embed(title=f'{cog} is not loaded.'))
+    except commands.ExtensionNotFound:
+        await ctx.send(embed=discord.Embed(title=f'Cog "{cog}" does not exist.'))
+
+@functions.utils.banned()
+@bot.command()
+async def coglist(ctx):
+    fileAmount = len(os.listdir('./cogs'))
+    cogList = ""
+    i = 0
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            i += 1
+            cogList = cogList + f"{filename[:-3]}, "
+    await ctx.send(embed=discord.Embed(title=f'{i} cogs', description=cogList, color=0xFF0000))
 
 @functions.utils.admin()
 @bot.command()
