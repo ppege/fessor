61f30c9be42088d88c4b15b90f80ff44fcae59cc
NanguRepo <56510257+NanguRepo@users.noreply.github.com>
2021-08-25 11:41:39 +0200

    Updates (#30)
    * Update README.md
    
    * add ability to bury privately and add auto-generating data.json file
    
    * cleanup
    
    * remove trailing whitespace

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# fessor
+# <p align="center">Fessor</p>
 
+[![Version Badge](https://img.shields.io/github/v/tag/nangurepo/fessor?label=version)](https://img.shields.io/github/v/tag/nangurepo/fessor?label=version)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/97c57bb49e0f4fbe94b38967169f17c4)](https://app.codacy.com/gh/NanguRepo/fessor?utm_source=github.com&utm_medium=referral&utm_content=NanguRepo/fessor&utm_campaign=Badge_Grade_Settings)
+[![License Badge](https://img.shields.io/github/license/nangurepo/fessor)](https://img.shields.io/github/license/nangurepo/fessor)
+[![Build Badge](https://img.shields.io/github/workflow/status/nangurepo/fessor/Pylint)](https://img.shields.io/github/workflow/status/nangurepo/fessor/Pylint)
+[![Checks Badge](https://img.shields.io/github/checks-status/nangurepo/fessor/v2.0.1)](https://img.shields.io/github/checks-status/nangurepo/fessor/v2.0.1)
+[![Lines Badge](https://img.shields.io/tokei/lines/github/nangurepo/fessor)](https://img.shields.io/tokei/lines/github/nangurepo/fessor)
+[![Redditor Badge](https://img.shields.io/reddit/user-karma/combined/nangu_?label=reddit%20karma)](https://reddit.com/u/nangu_)
 
-bot designed for the matematikfessor.dk discord server.
+Discord bot designed for the matematikfessor.dk discord server.
 
-## features
--  viggo scanner
--  simple prompt/response commands
--  permission system
--  slander gaming
--  moderation features
--  schedule manager
--  help command
-
-## coming soon
--  gif scanner
+## Features
+-   Viggo assignment scanner
+-   Permission system
+-   Moderation features
+-   Slash commands
+-   Chat burying! - A great, new, non-destructive alternative to purging chat
--- cogs/bury.py
+++ cogs/bury.py
@@ -3,14 +3,30 @@ from discord.ext import commands
 import functions.utils
 import discord_slash
 from discord_slash import cog_ext
+from discord_slash.utils.manage_commands import create_option
 
 class Bury(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="bury", description="Buries the chat!", guild_ids=functions.utils.servers, default_permission=False, permissions=functions.utils.slPerms("bury"))
-    async def bury(self, ctx: discord_slash.SlashContext):
-      await ctx.send('‌\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n‌')
+    @cog_ext.cog_slash(name="bury",
+                        description="Buries the chat!",
+                        guild_ids=functions.utils.servers,
+                        default_permission=False,
+                        permissions=functions.utils.slPerms("bury"),
+                        options=[
+                            create_option(
+                                name="private",
+                                description="send the message privately?",
+                                option_type=5,
+                                required=False
+                                )
+                            ]
+                        )
+    async def bury(self, ctx: discord_slash.SlashContext, **kwargs):
+        ephemeral=functions.utils.eCheck(**kwargs)
+        bury = '‌\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n‌'
+        await ctx.send(content=bury, hidden=ephemeral)
 
 def setup(bot):
     bot.add_cog(Bury(bot))
