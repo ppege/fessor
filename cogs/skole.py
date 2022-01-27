f0221da013d038f7692c275c296ee21824464705
NanguRepo <nanguthenangu@gmail.com>
2022-01-27 01:09:17 +0100

    use viggoscrape for homework scanning

--- cogs/skole.py
+++ cogs/skole.py
@@ -3,11 +3,11 @@ import json
 import datetime
 import configparser
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 import discord_slash
 from discord_slash import cog_ext
 from discord_slash.utils.manage_commands import create_option
-from viggoscrape import get_assignments
+from viggoscrape import Viggoscrape
 import functions.utils # pylint: disable=import-error
 from configs import options # pylint: disable=import-error
 
@@ -17,113 +17,35 @@ class Skole(commands.Cog):
         self.bot = bot # pylint: disable=no-member
         config = configparser.ConfigParser()
         config.read('cred.ini')
-        if config['config']['mode'] == "main":
-            self.scan_loop.start() # pylint: disable=no-member
-
-    def cog_unload(self):
-        """Unloads the cog."""
-        self.scan_loop.cancel() # pylint: disable=no-member
-
-
-    @tasks.loop(minutes=5.0)
-    async def scan_loop(self):
-        """Runs every five minutes and checks if the lektiescanner's output is different from the last."""
-        config = configparser.ConfigParser()
-        config.read('cred.ini')
-        keys = {
-            "USERNAME": config['config']['USERNAME'],
-            "PASSWORD": config['config']['PASSWORD']
-        }
-        assignment_data = json.loads(get_assignments("nr-aadal", keys))
-        selection = []
-        with open("data/scans.json", "r") as file:
-            data = json.load(file)
-        try:
-            for i in enumerate(assignment_data['description']):
-                i = i[0]
-                if assignment_data['description'][i] in data['scans']['description']:
-                    continue
-                data['scans']['subject'].append(assignment_data['subject'][i])
-                data['scans']['description'].append(assignment_data['description'][i])
-                data['scans']['author'].append(assignment_data['author'][i])
-                data['scans']['files'].append(assignment_data['files'][i])
-                data['scans']['time'].append(assignment_data['time'][i])
-                data['scans']['file_names'].append(assignment_data['file_names'][i])
-                data['scans']['url'].append(assignment_data['url'][i])
-                selection.append(i)
-        except KeyError:
-            data['scans']['subject'] = []
-            data['scans']['description'] = []
-            data['scans']['author'] = []
-            data['scans']['files'] = []
-            data['scans']['time'] = []
-            data['scans']['file_names'] = []
-            for i in enumerate(assignment_data['description']):
-                i = i[0]
-                if assignment_data['description'][i] in data['scans']['description']:
-                    continue
-                data['scans']['subject'].append(assignment_data['subject'][i])
-                data['scans']['description'].append(assignment_data['description'][i])
-                data['scans']['author'].append(assignment_data['author'][i])
-                data['scans']['files'].append(assignment_data['files'][i])
-                data['scans']['time'].append(assignment_data['time'][i])
-                data['scans']['file_names'].append(assignment_data['file_names'][i])
-                data['scans']['url'].append(assignment_data['url'][i])
-                selection.append(i)
-
-
-        with open("data/scans.json", "w") as file:
-            json.dump(data, file, indent=4)
-
-        if selection == []:
-            return
-        channel = self.bot.get_channel(816691188908294205)
-        await self.post(channel, assignment_data, selection)
+        self.scraper = Viggoscrape(
+            subdomain="nr-aadal",
+            username=config['config']['USERNAME'],
+            password=config['config']['PASSWORD']
+        )
 
     @staticmethod
-    async def post(ctx, assignment_data, selection):
+    async def post(ctx, assignment_data):
         """Formats assignment data to an embed then posts it to the channel in which the scan command was used."""
         with open("configs/assets.json", "r") as file:
-            data = json.load(file)
-        try:
-            print(selection[0])
-        except IndexError:
-            return "fail"
-        for i in selection:
-            current_class = assignment_data['subject'][i]
-            try:
-                embed_color = options.scanColors[current_class]
-            except KeyError:
-                embed_color = 0xFF5733
-            author = assignment_data['author'][i]
+            assets = json.load(file)
+        embeds = []
+        for assignment in assignment_data:
+            current_class = assignment['subject']
+            embed_color = options.scanColors[current_class] if current_class in options.scanColors else 0xFF5733
+            author = assignment['author']
             current_teacher = author.replace(" ".join(author.split(' ')[:4]), '').lstrip()
-            current_post_date = " ".join(author.split(' ')[:3])
-            try:
-                embed_thumbnail = data['teachers'][current_teacher.lower()]
-            except:
+            embed_thumbnail = assets['teachers'].get(current_teacher.lower())
+            if embed_thumbnail is None:
                 embed_thumbnail = "https://st3.depositphotos.com/4111759/13425/v/600/depositphotos_134255634-stock-illustration-avatar-icon-male-profile-gray.jpg"
-                raise
-            for_loop_files = [
-                assignment_data['files'][i].split(',')[j]
-                for j in range(len(assignment_data['files'][i].split(',')))
-            ]
 
-            for_loop_file_names = [
-                assignment_data['file_names'][i].split(',')[j]
-                for j in range(len(assignment_data['file_names'][i].split(',')))
-            ]
-
-            file_output = ""
-            for k in enumerate(for_loop_files):
-                k = k[0]
-                file_output = file_output + "[" + for_loop_file_names[k] + "](" + for_loop_files[k] + ")\n"
-            embed = discord.Embed(title=assignment_data['subject'][i], description=assignment_data['description'][i], color=embed_color, url=assignment_data['url'][i])
-            if file_output != "[None](None)\n":
-                embed.add_field(name="Files", value=file_output, inline=True)
-            embed.set_author(name=current_teacher, icon_url=embed_thumbnail, url="https://pypi.org/project/viggoscrape")
-            embed.set_footer(text=f"Posted on {current_post_date}\nDue on {assignment_data['time'][i]}")
-            await ctx.send(embed=embed)
-        return "success"
+            embed = discord.Embed(title=assignment['subject'], description=f"**Due on {assignment['date']}**\n{assignment['description']}", color=embed_color, url=assignment['url'])
+            embed.set_footer(text=f"{author}", icon_url=embed_thumbnail)
+            embeds.append(embed)
+        for i, embed in enumerate(embeds):
+            if i == 0:
+                await ctx.send(embed=embed)
+            else:
+                await ctx.channel.send(embed=embed)
 
     @cog_ext.cog_subcommand(
         base="schedule",
@@ -193,136 +115,66 @@ class Skole(commands.Cog):
         lektie_list = [
             str(i + 1)
             + ". "
-            + assignment_data['subject'][i]
-            + " | Afleveres "
-            + assignment_data['time'][i]
-            for i in range(len(assignment_data['subject']))
+            + assignment['subject']
+            + " | Due on "
+            + assignment['date']
+            for i, assignment in enumerate(assignment_data)
         ]
 
         description = "\n\n".join(lektie_list)
-        field_2 = "Use the command again with a different mode to see full assignments"
-        embed = discord.Embed(title="Found %d assignments" % len(assignment_data['subject']), description=description, color=0xFF0000)
+        field_2 = "Use the command with search to see full assignments"
+        embed = discord.Embed(title=f'Found {len(assignment_data)} assignments', description=description, color=0xFF0000)
         embed.add_field(name="What now?", value=field_2)
         await ctx.send(embed=embed)
         return
 
-    @staticmethod
-    def find_subjects(assignment_data, parameters):
-        """Finds assignments of a specific subject."""
-        return [
-            i
-            for i in range(len(assignment_data['subject']))
-            if parameters in assignment_data['subject'][i]
+    def filter_search(self, data, search):
+        """Use viggoscrape to get assignment data"""
+        def match_search(assignment):
+            return any(search in value for value in assignment.values())
+        return list(filter(match_search, data))
+
+    @cog_ext.cog_slash(
+        name="homework",
+        description="Check if there's homework due",
+        guild_ids=functions.utils.servers,
+        default_permission=False,
+        permissions=functions.utils.slash_perms("lektiescan"),
+        options=[
+            create_option(
+                name="search",
+                description="Which teacher, subject, day, etc.",
+                option_type=3,
+                required=False
+            )
         ]
-
-    @staticmethod
-    def find_dates(assignment_data, parameters):
-        """Finds assignments from a specific date."""
-        if parameters == "tomorrow":
-            tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-            tomorrow = tomorrow.strftime("%d. %b").replace('May', 'Maj').replace('Oct', 'Okt').replace('0', '').lower()
-            return [
-                i
-                for i in range(len(assignment_data['time']))
-                if str(tomorrow) in assignment_data['time'][i]
-            ]
-
-        if (
-            parameters not in options.translations.keys()
-            and parameters not in options.translations.values()
-        ):
-            return [
-                i
-                for i in range(len(assignment_data['subject']))
-                if str(parameters) in assignment_data['time'][i]
-            ]
-
-        weekday = datetime.datetime.today().weekday()
-        if parameters in options.translations.keys():
-            parameters = options.translations[parameters]
-        diff = weekday - int(options.conversions[parameters])
-        target_date = datetime.date.today() - datetime.timedelta(days=diff)
-        target_date = target_date.strftime("%d. %b").replace('May', 'Maj').replace('Oct', 'Okt').replace('0', '').lower()
-        user_input = target_date
-        return [
-            i
-            for i in range(len(assignment_data['time']))
-            if str(user_input) in assignment_data['time'][i]
-        ]
-
-    @staticmethod
-    def find_teacher(assignment_data, parameters):
-        """Finds assignments from a specific teacher."""
-        return [
-            i
-            for i in range(len(assignment_data['author']))
-            if str(parameters) in assignment_data['author'][i]
-        ]
-
-    async def handle_mode(self, ctx, assignment_data, mode, parameters):
-        """Handles mode and parameters to know what to look for."""
-        if mode == "all":
-            await self.send_all(ctx, assignment_data)
-            return None
-        if mode == "subject":
-            user_input = self.find_subjects(assignment_data, parameters)
-        elif mode == "date":
-            user_input = self.find_dates(assignment_data, parameters)
-        elif mode == "teacher":
-            user_input = self.find_teacher(assignment_data, parameters)
-        return user_input
-
-    async def scan(self, ctx: discord_slash.SlashContext, **kwargs):
-        """Function that obtains raw data from the lektiescanner function and formats the text, then calls post."""
-        ephemeral = functions.utils.ephemeral_check(**kwargs)
-        if 'parameters' not in kwargs:
-            kwargs['parameters'] = None
-        try:
-            await ctx.defer(hidden=ephemeral)
-            config = configparser.ConfigParser()
-            config.read('cred.ini')
-            keys = {
-                "USERNAME": config['config']['USERNAME'],
-                "PASSWORD": config['config']['PASSWORD']
-            }
-            assignment_data = json.loads(get_assignments("nr-aadal", keys))
-            user_input = await self.handle_mode(ctx, assignment_data, kwargs['mode'], kwargs['parameters'])
-            if str(user_input) == "[]":
-                await ctx.send(embed=discord.Embed(title='No assignments found :weary:', color=0xFF0000))
-                return
-            if user_input is None:
-                return
-            await self.post(ctx, assignment_data, user_input)
-        except Exception as error:
-            await ctx.send(
+    )
+    async def homework(self, ctx: discord_slash.SlashContext, **kwargs):
+        """Command to get assignments, powered by viggoscrape"""
+        data = self.scraper.get_assignments()
+        if "errors" in data:
+            ctx.send(
                 embed=discord.Embed(
-                    title='Scan failed.',
-                    description=f'Exception:\n```\n{error}\n```',
-                    color=16711680,
+                    title="Error",
+                    description="\n".join(data["errors"]),
+                    color=0xFF0000
                 )
             )
-            raise
-
-    @cog_ext.cog_subcommand(base="scan", name="all", description="Scan for an index of all assignments on Viggo.", guild_ids=functions.utils.servers, base_default_permission=False, base_permissions=functions.utils.slash_perms("lektiescan"), options=[create_option(name="private", description="send the message privately?", option_type=5, required=False)])
-    async def _scan_all(self, ctx: discord_slash.SlashContext, **kwargs):
-        ephemeral = functions.utils.ephemeral_check(**kwargs)
-        await self.scan(ctx, mode="all", private=ephemeral)
-
-    @cog_ext.cog_subcommand(base="scan", name="subject", description="Scan for assignments from a specific subject on Viggo.", guild_ids=functions.utils.servers, options=[create_option(name="subject", description="Which subject?", option_type=3, required=True), create_option(name="private", description="send the message privately?", option_type=5, required=False)])
-    async def _scan_subject(self, ctx: discord_slash.SlashContext, **kwargs):
-        ephemeral = functions.utils.ephemeral_check(**kwargs)
-        await self.scan(ctx, mode="subject", parameters=kwargs["subject"], private=ephemeral)
-
-    @cog_ext.cog_subcommand(base="scan", name="date", description="Scan for assignments from a specific date on Viggo.", guild_ids=functions.utils.servers, options=[create_option(name="date", description="Which date?", option_type=3, required=True), create_option(name="private", description="send the message privately?", option_type=5, required=False)])
-    async def _scan_date(self, ctx: discord_slash.SlashContext, **kwargs):
-        ephemeral = functions.utils.ephemeral_check(**kwargs)
-        await self.scan(ctx, mode="date", parameters=kwargs["date"], private=ephemeral)
-
-    @cog_ext.cog_subcommand(base="scan", name="teacher", description="Scan for assignments from a specific teacher on Viggo.", guild_ids=functions.utils.servers, options=[create_option(name="teacher", description="Which teacher?", option_type=3, required=True), create_option(name="private", description="send the message privately?", option_type=5, required=False)])
-    async def _scan_teacher(self, ctx: discord_slash.SlashContext, **kwargs):
-        ephemeral = functions.utils.ephemeral_check(**kwargs)
-        await self.scan(ctx, mode="teacher", parameters=kwargs["teacher"], private=ephemeral)
-
+            return
+        searching = False
+        if "search" in kwargs:
+            data = self.filter_search(data, kwargs["search"])
+            searching = True
+        if data == []:
+            await ctx.send(embed=discord.Embed(title="No assignments found."))
+            return
+        match searching:
+            case True:
+                await self.post(ctx, data)
+            case False:
+                await self.send_all(ctx, data)
+            case _:
+                await ctx.send(embed=discord.Embed(title="Critical error?!?! check logs :rage:"))
 
 def setup(bot):
     """Adds the cog."""
--- fallback.py
+++ fallback.py
@@ -1,4 +1,4 @@
-"""Ran when ctx.py recieves the shutdown or restart command, it handles user ability to start the bot again."""
+"""Ran when main.py recieves the shutdown or restart command, it handles user ability to start the bot again."""
 import os
 import sys
 import subprocess
@@ -16,8 +16,7 @@ async def on_ready():
     print('FALLBACK READY.')
     print(sys.argv)
     if sys.argv[1] == "restart":
-        subprocess.Popen(['python3', 'ctx.py'])
-        print('blah')
+        subprocess.Popen(['python3', 'main.py'])
         channel = bot.get_channel(int(sys.argv[2]))
         await channel.send(embed=discord.Embed(title='Bot started.', color=0xFF0000))
         sys.exit(0)
@@ -27,21 +26,13 @@ async def start(ctx):
     """Command to start the bot again."""
     if ctx.author.id == 273845229130481665:
         message = await ctx.send(embed=discord.Embed(title='Starting bot...', description='', color=0xFFFFFF))
-        subprocess.Popen(['python3', 'ctx.py'])
+        subprocess.Popen(['python3', 'main.py'])
         await message.edit(embed=discord.Embed(title='Bot started.', description='', color=0xFF0000))
         sys.exit(0)
 
 @bot.command()
-async def update(ctx):
-    """Command to update the bot using git; this command is deprecated - use /update instead."""
-    if ctx.author.id == 273845229130481665:
-        await ctx.send(embed=discord.Embed(title='Updating bot...', description='', color=0xFF0000))
-        os.system('git pull')
-
-@bot.command()
 async def ping(ctx):
     """Ping command so the user can see if the script is running."""
-    print('fallback pinged')
     await ctx.send('pong!')
 
 config = configparser.ConfigParser()
